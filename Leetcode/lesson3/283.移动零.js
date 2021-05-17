4b000656810a42abe1892183ce97058d8fb21f99
zhLeon521 <2321837783@qq.com>
2021-05-17 23:56:17 +0800

    移动零，双指针方法

---
+++ Leetcode/lesson3/283.移动零.js
@@ -0,0 +1,42 @@
+/*
+ * @Description: 移动0
+ * @Autor: Blueheart
+ * @Date: 2021-05-17 23:21:47
+ * @LastEditTime: 2021-05-17 23:55:56
+ * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\lesson3\283.移动零.js
+ */
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * 思路：
+ * 定义快慢指针，fast负责遍历nums
+ * 1.若fast指向非0，slow+1;
+ * 2.若fast指向0，slow不变
+ *      fast继续移动，直到fast指向非0，交换连个指针。slow+1
+ * 
+ *简记：不为零就交换，slow++; 为零就不交换，slow不动
+ */
+
+let nums = [0, 1, 0, 3, 0,12];
+var moveZeroes = function (nums) {
+    let fast = 0, slow = 0;
+    while (fast < nums.length) {
+        if (nums[fast] !== 0) {
+            // ES6解构； 交换交换两个数组方式 [a,b] = [b,a]；
+            [nums[slow],nums[fast]] =[nums[fast],nums[slow]];
+            slow++;
+        } fast++;
+    }   
+    console.log(nums);
+    return nums;
+    
+};
+// @lc code=end
+moveZeroes(nums)
