e0d8e2034683345279734e8f59ee86f224692016
zhLeon521 <2321837783@qq.com>
2021-05-17 11:48:10 +0800

    合并两个数组，从后面插入

--- Leetcode/lesson3/88.合并两个有序数组.js
+++ Leetcode/lesson3/88.合并两个有序数组.js
@@ -2,7 +2,7 @@
  * @Description: 合并两个有序数组
  * @Autor: Blueheart
  * @Date: 2021-05-17 10:35:04
- * @LastEditTime: 2021-05-17 11:05:52
+ * @LastEditTime: 2021-05-17 11:33:12
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\lesson3\88.合并两个有序数组.js
  */
 /*
@@ -36,9 +36,37 @@
  */
 
 var merge = function (nums1, m, nums2, n) {
-    // 方法1：直接添加，然后排序
-    nums1.splice(m, nums1.length - m, ...nums2);
-    nums1.sort((a, b) => a - b);
+    // // 方法1：直接添加，然后排序
+    // nums1.splice(m, nums1.length - m, ...nums2);
+    // nums1.sort((a, b) => a - b);
+
+
+
+
+    // 方法2:从后面操作
+    // 因为是有序数组，所以对应位置进行比较。返回的是nums1
+    // 如果nums比较完之后，nums2还有元素，就是直接追加就好
+    
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    while (j >= 0) {
+        nums1[k--] = nums2[j--]
+    }
+    
+    
+    
+    
+    
 };
 // @lc code=end
 
