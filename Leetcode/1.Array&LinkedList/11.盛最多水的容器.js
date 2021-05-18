08da14c9edde8f3b9aa1246db8dc176c4fa4b24d
zhLeon521 <2321837783@qq.com>
2021-05-19 00:40:51 +0800

    11添加思路

--- Leetcode/1.Array&LinkedList/11.盛最多水的容器.js
+++ Leetcode/1.Array&LinkedList/11.盛最多水的容器.js
@@ -2,7 +2,7 @@
  * @Description: 盛水最多的容器
  * @Autor: Blueheart
  * @Date: 2021-05-19 00:17:52
- * @LastEditTime: 2021-05-19 00:35:14
+ * @LastEditTime: 2021-05-19 00:40:31
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\1.Array&LinkedList\11.盛最多水的容器.js
  */
 /*
@@ -15,6 +15,10 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * 思路：
+ * 两个指针，一个在索引0处，一个在索引最后位置（长度减一位置：height.length-1）
+ * 容器的长度为j-i;高度为min(height[i],height[j])===>面积res = (j-i)*矮的那根柱子
+ * 然后矮的那根柱子向内部移动，比较所有的面积就可！
  */
 
 // height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
