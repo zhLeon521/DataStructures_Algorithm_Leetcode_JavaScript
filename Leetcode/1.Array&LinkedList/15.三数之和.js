a46f9bd76494464ec58707016ec93690f344d7b0
zhLeon521 <2321837783@qq.com>
2021-05-19 21:52:38 +0800

    15,三数之和，添加做题思路

--- Leetcode/1.Array&LinkedList/15.三数之和.js
+++ Leetcode/1.Array&LinkedList/15.三数之和.js
@@ -2,7 +2,7 @@
  * @Description: 三数之和
  * @Autor: Blueheart
  * @Date: 2021-05-19 10:53:21
- * @LastEditTime: 2021-05-19 21:40:35
+ * @LastEditTime: 2021-05-19 21:52:13
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\1.Array&LinkedList\15.三数之和.js
  */
 /*
@@ -15,6 +15,15 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
+ * 思路：
+ * 三数之和，预判断：
+ * 首先就是数组为空，或者不满三个元素，直接返回空数组[]
+ * 1.排序(从小到大) nums.sort((a,b)=>a-b)  || 如果第一个元素大于0(相加不可能大于0了)
+ * 2.固定第一个元素（然后去重），然后从i= K+1;j = nums.length-1. 两面夹击开始运算
+ * 3.和等于零，对i,j都得进行去重处理。把元素里的值push进去
+ * 4. 不等于零，sum小于零就移动i;大于0就移动j;
+ * 
+ * 
  */
 let nums = [-1, 0, 1, 2, -1, -4];
 
@@ -30,7 +39,7 @@ var threeSum = function (nums) {
 
     for (let k = 0; k < len - 2; k++){
         if (nums[k] > 0) break; //如果排序后的第一个元素大于0，则和肯定大于零
-        if (k > 0 && nums[k] == nums[k - 1]) continue;
+        if (k > 0 && nums[k] == nums[k - 1]) continue;  //去重
         let i = k + 1, j = nums.length - 1; //i,j都是下标
         while (i < j) {
             const sum = nums[k] + nums[i] + nums[j];
