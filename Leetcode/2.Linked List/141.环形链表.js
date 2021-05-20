5c1f2f72397530271d807eb227ea5d5c7898a4a2
zhLeon521 <2321837783@qq.com>
2021-05-20 16:01:37 +0800

    141.环形链表

---
+++ Leetcode/2.Linked List/141.环形链表.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function (head) {
+    let slow = head;
+    let fast = head;
+    if (head === null) { //空的，肯定没环
+        return false;
+    }
+    // 判断一个节点，两个节点不为空，就有戏
+    while (fast.next !== null && fast.next.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow.val === fast.val) {
+            return true;
+        }
+     }
+     return false;
+
+};
+// @lc code=end
+
