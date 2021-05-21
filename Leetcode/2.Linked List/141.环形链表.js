e5ee1d88f7f592a50eb84c2857b43d1956305065
zhLeon521 <2321837783@qq.com>
2021-05-21 10:41:40 +0800

    141.在练习一遍

--- Leetcode/2.Linked List/141.环形链表.js
+++ Leetcode/2.Linked List/141.环形链表.js
@@ -16,23 +16,22 @@
 /**
  * @param {ListNode} head
  * @return {boolean}
- * 龟兔赛跑原理
  */
- var hasCycle = function (head) {
-    let slow = head;
-    let fast = head;
-    if (head === null) { //空的，肯定没环
+var hasCycle = function (head) {
+    if (head === null) {
         return false;
     }
-    // 判断一个节点，两个节点不为空，就有戏
+    let fast = head;
+    let slow = head;
     while (fast.next !== null && fast.next.next !== null) {
-        slow = slow.next; //一格
-        fast = fast.next.next; //两格
-        if (slow === fast) { //两个撞上了
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast === slow) {
             return true;
         }
-     }
-     return false;
-
+    }
+    return false;
+    
 };
 // @lc code=end
+
