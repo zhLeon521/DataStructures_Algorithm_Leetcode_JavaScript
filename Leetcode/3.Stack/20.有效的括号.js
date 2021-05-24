5fcff92aa3dbf5aa8be7d3ef103988050125a050
zhLeon521 <2321837783@qq.com>
2021-05-24 11:41:41 +0800

    20.解题思路

--- Leetcode/3.Stack/20.有效的括号.js
+++ Leetcode/3.Stack/20.有效的括号.js
@@ -2,7 +2,7 @@
  * @Description: 有效的括号
  * @Autor: Blueheart
  * @Date: 2021-05-24 11:24:28
- * @LastEditTime: 2021-05-24 11:32:16
+ * @LastEditTime: 2021-05-24 11:39:14
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\3.Stack\20.有效的括号.js
  */
 /*
@@ -15,6 +15,14 @@
 /**
  * @param {string} s
  * @return {boolean}
+ * 
+ * 解题步骤：
+ * 1.建立一个HashMap,把括号配对放进去
+ * 2.创建一个Stack(Array),for循环遍历字符串。
+ * 如果map中有key ,说明他是左括号，从map 中取出对应的右括号放到Stack中。
+ * 否则的话他就是右括号，这时需要将stack 栈顶元素弹出看是否相等，不相等返回false
+ * 
+ * 3.循环结束后，看一下stack是否为空，不为空就说明还剩一些左括号没有被闭合，返回false.否则返回true。
  */
 var isValid = function (s) {
     const map = new Map();
