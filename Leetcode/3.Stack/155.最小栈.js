0e7c976c6822b45dabb1ccda4e5909ed56b69860
zhLeon521 <2321837783@qq.com>
2021-05-24 19:14:07 +0800

    还么想好

---
+++ Leetcode/3.Stack/155.最小栈.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=javascript
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
+
