1847b35e8caa4bae4e9e7fbec839317f6b2cad3e
zhLeon521 <2321837783@qq.com>
2021-05-27 17:08:14 +0800

    88.后插法

--- leetcode-2/1.Array/88.合并两个有序数组.js
+++ leetcode-2/1.Array/88.合并两个有序数组.js
@@ -2,7 +2,7 @@
  * @Description: 合并两个有序数组
  * @Autor: Blueheart
  * @Date: 2021-05-27 16:48:05
- * @LastEditTime: 2021-05-27 16:53:50
+ * @LastEditTime: 2021-05-27 17:07:55
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\leetcode-2\1.Array\88.合并两个有序数组.js
  */
 /*
@@ -20,9 +20,26 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    nums1.splice(m, nums1.length - m, ...nums2);
-    nums1.sort((a, b) => a - b);
-    return nums1;
+    // nums1.splice(m, nums1.length - m, ...nums2);
+    // nums1.sort((a, b) => a - b);
+    // return nums1;
+
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+ 
+
 
 };
 // @lc code=end
