827220be47066e7562bf18cf1a29ab0d0e14b4ca
zhLeon521 <2321837783@qq.com>
2021-07-20 23:36:51 +0800

    用栈实现队列

---
+++ Leetcode/4.Queue/232.用栈实现队列.js
@@ -0,0 +1,82 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2021-07-20 23:21:43
+ * @LastEditTime: 2021-07-20 23:35:10
+ * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\4.Queue\232.用栈实现队列.js
+ */
+/*
+ * @lc app=leetcode.cn id=232 lang=javascript
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+    this.stack1 = [];
+    this.stack2 = [];
+    
+
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.stack1.push(x);
+
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    if (this.stack2.length <= 0) {
+        while (this.stack1.length != 0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    // 只从stack2中出栈元素
+    return this.stack2.pop();
+    
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+    if (this.stack2.length <= 0) {
+        while (this.stack1.length != 0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    const stack2Len = this.stack2.length;
+    return stack2Len && this.stack2[stack2Len - 1];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return !this.stack1.length && !this.stack2.length;
+
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
+
