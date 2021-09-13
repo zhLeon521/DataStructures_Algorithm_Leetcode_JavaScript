cabd413cd4f4603c8f3eedce850e828a80fab009
zhLeon521 <2321837783@qq.com>
2021-09-13 10:50:25 +0800

    14 简化了一下

--- Leetcode/1.Array/14.最长公共前缀.js
+++ Leetcode/1.Array/14.最长公共前缀.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-09-11 14:59:36
- * @LastEditTime: 2021-09-13 10:10:07
+ * @LastEditTime: 2021-09-13 10:50:06
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\1.Array\14.最长公共前缀.js
  */
 /*
@@ -21,26 +21,35 @@
  * 只比较第一个和最后一个，相等就继续走，不相等就返回
  * 
  */
+// var longestCommonPrefix = function (strs) {
+
+//     let ans = '';
+//     // 按字母顺序排列数组
+//     if (!strs || !strs.length) return '';
+
+//     strs.sort();
+
+//     // 按照第一个元素长度进行遍历；
+//     for (let i = 0; i < strs[0].length; ++i){
+//         if (strs[0][i] !== strs[strs.length - 1][i]) { //只比较第一项和最后一项
+//             break;
+//         } else {
+//             ans += strs[0][i];
+//         }
+//     }
+
+//     return ans;
+
+// };
+
 var longestCommonPrefix = function (strs) {
-
-    let ans = '';
-    // 按字母顺序排列数组
-    if (!strs || !strs.length) return '';
-
+    
     strs.sort();
-
-    // 按照第一个元素长度进行遍历；
     for (let i = 0; i < strs[0].length; ++i){
-        if (strs[0][i] !== strs[strs.length - 1][i]) { //只比较第一项和最后一项
-            break;
-        } else {
-            ans += strs[0][i];
-        }
+        // substring() 方法返回一个字符串在开始索引到结束索引之间的一个子集
+        if(strs[0][i] !== strs[strs.length - 1][i]) return strs[0].substring(0,i)
     }
-
-    return ans;
-
-
+    return strs[0];
 
 };
 // @lc code=end
