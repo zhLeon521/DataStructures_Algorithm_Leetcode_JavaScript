17589daa97d0f07a33ec429f03479cbf13c092fa
zhLeon521 <2321837783@qq.com>
2021-11-04 18:00:18 +0800

    留着以后做 dfs 回溯

---
+++ Leetcode/2.Linked List/138.复制带随机指针的链表.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=javascript
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    
+};
+// @lc code=end
+
---
+++ Leetcode/2.Linked List/430.扁平化多级双向链表.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=430 lang=javascript
+ *
+ * [430] 扁平化多级双向链表
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+    
+};
+// @lc code=end
+
