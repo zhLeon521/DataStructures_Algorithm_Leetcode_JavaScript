d884dcac60d0f0d8f50f0586fd80ce41a1466371
zhLeon521 <2321837783@qq.com>
2021-11-06 11:10:42 +0800

    迭代 中序遍历

--- Leetcode/5.Tree/94.二叉树的中序遍历.js
+++ Leetcode/5.Tree/94.二叉树的中序遍历.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-11-06 09:52:17
- * @LastEditTime: 2021-11-06 09:57:39
+ * @LastEditTime: 2021-11-06 11:09:55
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\5.Tree\94.二叉树的中序遍历.js
  */
 /*
@@ -24,27 +24,52 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function (root) {
-    let res = [];
-    const order = function (root) {
-        if (root ===  null) return res;
+// var inorderTraversal = function (root) {
+//     let res = [];
+//     const order = function (root) {
+//         if (root ===  null) return res;
 
-        if (root.left !== null) {
-            order(root.left);
-        }
+//         if (root.left !== null) {
+//             order(root.left);
+//         }
 
-        res.push(root.val);
+//         res.push(root.val);
 
-        if (root.right !== null) {
-            order(root.right);
-        }
+//         if (root.right !== null) {
+//             order(root.right);
+//         }
         
+//     }
+    
+//     order(root);
+//     return res;
+    
+
+// };
+
+var inorderTraversal = function (root) {
+    const res = [];
+    if (root === null) return res;
+
+    const stack = [];
+    let curr = root;
+
+
+    while (stack.length || curr) {
+        // 不断将左节点压入
+        if (curr !== null) {
+            stack.push(curr);
+            curr = curr.left;
+        } else {
+            // --> 弹出 中
+            curr = stack.pop();
+            res.push(curr.val)
+            // 右
+            curr = curr.right;
+        }
     }
     
-    order(root);
     return res;
-    
-
-};
+}
 // @lc code=end
 
