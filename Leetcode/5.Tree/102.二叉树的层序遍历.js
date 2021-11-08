82dd96901aab15bc00182341a68e3eb8dc69ed36
zhLeon521 <2321837783@qq.com>
2021-11-08 10:30:10 +0800

    层序遍历

--- Leetcode/5.Tree/102.二叉树的层序遍历.js
+++ Leetcode/5.Tree/102.二叉树的层序遍历.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2021-11-08 10:04:27
- * @LastEditTime: 2021-11-08 10:13:55
+ * @LastEditTime: 2021-11-08 10:29:45
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\Leetcode\5.Tree\102.二叉树的层序遍历.js
  */
 /*
@@ -25,35 +25,31 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    let res = [];
+    let res= [];
+    let queue = [];
+
     if (root === null) return res;
 
-    const queue = [];
     queue.push(root);
-
     while (queue.length) {
-        const length = queue.length; //记录当前层数的节点数
-        const currLevel = []; //存放每一层的节点
+        const length = queue.length;
+        let currLevel = [];
 
         for (let i = 0; i < length; i++){
-            let node = queue.shift();  //第一个节点打入队列
+            let node = queue.shift(); //取第一个元素
             currLevel.push(node.val);
-
             if (node.left) {
-                queue.push(node.left)
+                queue.push(node.left);
             }
+
             if (node.right) {
-                queue.push(node.right);
+                queue.push(node.right)
             }
+            
         }
-
-        res.push(currLevel);
-        
+        res.push(currLevel)
     }
-
     return res;
-
-
 
 };
 // @lc code=end
