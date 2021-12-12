852229a1177da248c47a434a898d32458c7e5ebb
blueheart <2321837783@qq.com>
2021-12-12 13:07:38 +0800

    左叶子之和

--- Leetcode/5.Tree/404.左叶子之和.js
+++ Leetcode/5.Tree/404.左叶子之和.js
@@ -17,27 +17,57 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// var sumOfLeftLeaves = function (root) {
+
+//     const nodeSum = (node) => {
+//         if (node === null) return 0;
+
+//         let leftValue = sumOfLeftLeaves(node.left)
+//         let rightValue = sumOfLeftLeaves(node.right)
+
+//         let midValue = 0;
+
+//         // 判断当前节点是不是左叶子是无法判断的，必须要通过节点的父节点来判断其左孩子是不是左叶子
+//         // 当遇到左叶子节点的时候，记录数值，然后通过递归求取左子树左叶子之和，和 右子树左叶子之和，相加便是整个树的左叶子之和。
+//         if (node.left && node.left.left === null && node.left.right === null) {
+//             midValue = node.left.val;
+//         }
+
+//         let sum = leftValue + rightValue + midValue;
+//         return sum;
+//     }
+//     return nodeSum(root);
+
+// };
+
+
+
+// 迭代
+// 采用层序遍历
 var sumOfLeftLeaves = function (root) {
+    let queue = [];
+    queue.push(root);
 
-    const nodeSum = (node) => {
-        if (node === null) return 0;
+    if (root === null) return 0;
+    let sum = 0;
 
-        let leftValue = sumOfLeftLeaves(node.left)
-        let rightValue = sumOfLeftLeaves(node.right)
-
-        let midValue = 0;
-
-        // 判断当前节点是不是左叶子是无法判断的，必须要通过节点的父节点来判断其左孩子是不是左叶子
-        // 当遇到左叶子节点的时候，记录数值，然后通过递归求取左子树左叶子之和，和 右子树左叶子之和，相加便是整个树的左叶子之和。
+    while (queue.length) {
+        let node = queue.shift();
         if (node.left && node.left.left === null && node.left.right === null) {
-            midValue = node.left.val;
+            sum += node.left.val
         }
-
-        let sum = leftValue + rightValue + midValue;
-        return sum;
+        if (node.left) {
+            queue.push(node.left)
+        }
+        if (node.right) {
+            queue.push(node.right)
+        }
     }
-    return nodeSum(root);
+    return sum;
 
-};
+
+
+
+}
 // @lc code=end
 
