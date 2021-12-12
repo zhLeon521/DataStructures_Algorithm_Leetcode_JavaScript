da4acd574fdca4958b906c181dfadc3ae9d0262a
blueheart <2321837783@qq.com>
2021-12-12 12:28:04 +0800

    左叶子节点和

---
+++ Leetcode/5.Tree/404.左叶子之和.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=javascript
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root) {
+
+    const nodeSum = (node) => {
+        if (node === null) return 0;
+
+        let leftValue = sumOfLeftLeaves(node.left)
+        let rightValue = sumOfLeftLeaves(node.right)
+
+        let midValue = 0;
+
+        // 判断当前节点是不是左叶子是无法判断的，必须要通过节点的父节点来判断其左孩子是不是左叶子
+        // 当遇到左叶子节点的时候，记录数值，然后通过递归求取左子树左叶子之和，和 右子树左叶子之和，相加便是整个树的左叶子之和。
+        if (node.left && node.left.left === null && node.left.right === null) {
+            midValue = node.left.val;
+        }
+
+        let sum = leftValue + rightValue + midValue;
+        return sum;
+    }
+    return nodeSum(root);
+
+};
+// @lc code=end
+
