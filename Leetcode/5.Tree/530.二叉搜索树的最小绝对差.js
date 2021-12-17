f109162c785a44d288e7eb977adff4fb562354a9
zhleon <zholeon@qq.com>
2021-12-17 14:07:38 +0800

    用指针记录

--- Leetcode/5.Tree/530.二叉搜索树的最小绝对差.js
+++ Leetcode/5.Tree/530.二叉搜索树的最小绝对差.js
@@ -40,28 +40,48 @@
 
 // };
 
-// 换个写法
-var getMinimumDifference = function (root) {
-    let arr = [];
-    const toArray = (root) => {
-        if (root) {
-            toArray(root.left);
-            arr.push(root.val);
-            toArray(root.right)
-        }
-    }
+// // 换个写法
+// var getMinimumDifference = function (root) {
+//     let arr = [];
+//     const toArray = (root) => {
+//         if (root) {
+//             toArray(root.left);
+//             arr.push(root.val);
+//             toArray(root.right)
+//         }
+//     }
 
-    toArray(root);
-    let res = Infinity; //输出结果
-    let i = 1; //准备遍历
-    while (i < arr.length) {
-        let diff = Math.abs(arr[i - 1] - arr[i]);
-        if (diff < res) {
-            res = diff;
+//     toArray(root);
+//     let res = Infinity; //输出结果
+//     let i = 1; //准备遍历
+//     while (i < arr.length) {
+//         let diff = Math.abs(arr[i - 1] - arr[i]);
+//         if (diff < res) {
+//             res = diff;
+//         }
+//         i++;
+//     }
+//     return res;
+// }
+
+
+// 使用prev节点指向所指节点的前一个节点
+var getMinimumDifference = function (root) {
+    let res = Infinity;
+    let prev = null
+
+    const traversal = (node) => {
+        if (node === null) return;
+        traversal(node.left);
+        if (prev !== null) {
+            res = Math.min(res, Math.abs(prev - node.val));
         }
-        i++;
+        prev = node.val;
+        traversal(node.right);
     }
+    traversal(root);
     return res;
 }
+
 // @lc code=end
 
