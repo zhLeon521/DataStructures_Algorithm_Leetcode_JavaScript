84b6a336ae22166eba7b48f29eaefba47e8c385e
zhleon <zholeon@qq.com>
2021-12-23 15:52:27 +0800

    电话号码字母组合

--- Leetcode/5.Tree/17.电话号码的字母组合.js
+++ Leetcode/5.Tree/17.电话号码的字母组合.js
@@ -9,63 +9,46 @@
  * @param {string} digits
  * @return {string[]}
  */
-// const letterCombinations = (digits) => {
-//     if (digits == null || digits.length === 0) return [];
 
-//     const map = {
-//         2: 'abc',
-//         3: 'def',
-//         4: 'ghi',
-//         5: 'jkl',
-//         6: 'mno',
-//         7: 'pqrs',
-//         8: 'tuv',
-//         9: 'wxyz',
-//     };
 
-//     const res = [];
-//     const backtracking = (i, s) => {
-//         if (i === digits.length) {
-//             res.push(s);
-//             return;
-//         }
-
-//         for (const c of map[digits[i]]) {
-//             backtracking(i + 1, s + c);
-//         }
-//     };
-
-//     backtracking(0, '');
-//     return res;
-// };
 
 const letterCombinations = (digits) => {
-    let len = digits.length;
-    // 没有传参，返回空
-    if (len === 0) return [];
-    //初始对应所有的数字，为了直接对应2-9，新增了两个无效的字符串""
-    const map = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
-    // 传参只有一个，就直接把这个格子里的给拆分开 
-    if (len === 1) return map[digits].split("");
+    if (digits.length === 0) return [];
+    const map = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz',
+    };
 
     let res = [];
     let path = [];
 
-    const backtracking = (digits, len, a) => {
-        if (path.length === len) {
-            res.push(path.join(""))
-            return;
+    // digits:传入的【数字】，如“23”，digits.length=2
+    // index:记录遍历第几个【数字】
+    const backtracking = (digits, index) => {
+        if (digits.length === index) { //如果此时index等于【输入数字的个数】
+            res.push(path.join("")) //将此条结果传入res中
+            return; //结束本层迭代
         }
-        for (const i of map[digits[a]]) {
-            path.push(i);
-            backtracking(digits, len, a + 1);
-            path.pop();
+        // 迭代循环：index
+        for (const letter of map[digits[index]]) {
+            path.push(letter)
+            backtracking(digits, index + 1) //递归， index+1下层要处理下一个数字了
+            path.pop() // 回溯
         }
+
     }
 
-    backtracking(digits, len, 0);
+    backtracking(digits, 0)
+
     return res;
 
-}
+
+};
 // @lc code=end
 
