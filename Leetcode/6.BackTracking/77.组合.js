f1bdd10ec63edba6249949ed7ad9041eb06a9ab4
zhleon <zholeon@qq.com>
2021-12-27 12:49:37 +0800

    加了一个非优化操作

--- Leetcode/6.BackTracking/77.组合.js
+++ Leetcode/6.BackTracking/77.组合.js
@@ -10,33 +10,55 @@
  * @param {number} k
  * @return {number[][]}
  */
+// var combine = function (n, k) {
+//     let res = []; // 存放所有结果
+//     let path = []; // 存放符合条件的结果
+
+
+//     const backtracking = (n, k, startIndex) => {
+//         // 如果已经选择的元素个数path.length等于k,直接将结果放到res中，并返回
+//         if (path.length === k) {
+//             // res.push([...path]) 
+//             res.push(path.slice()) //slice 是纯函数
+//             return;
+//         }
+//         // 横向遍历，在集合n中，至多要从（n - (k - path.length) + 1）开始遍历。这是一个左闭的区间
+//         // 举个例子：n=4, k=3。目前已经选取的元素个数为0（path.length===0）,那么 n-(k-path.length)+1 即 4-(3-0)+1=2，从2开始搜索是合理的，组合为【2,3,4】
+//         // +1 的意思是要包括起始位置，这是一个左闭的区间
+//         // i 为本次搜索的起始位置
+//         for (let i = startIndex; i <= n - (k - path.length) + 1; i++) {
+//             path.push(i);
+//             backtracking(n, k, i + 1);
+//             path.pop();
+//         }
+
+//     }
+
+
+//     backtracking(n, k, 1)
+//     return res;
+
+
+// };
+
+// 暴力法， 没有剪枝操作
 var combine = function (n, k) {
-    let res = []; // 存放所有结果
-    let path = []; // 存放符合条件的结果
-
-
+    let res = [];
+    let path = [];
     const backtracking = (n, k, startIndex) => {
-        // 如果已经选择的元素个数path.length等于k,直接将结果放到res中，并返回
         if (path.length === k) {
-            // res.push([...path]) 
-            res.push(path.slice()) //slice 是纯函数
+            res.push(path.slice());
             return;
         }
-        // 横向遍历，在集合n中，至多要从（n - (k - path.length) + 1）开始遍历。这是一个左闭的区间
-        // 举个例子：n=4, k=3。目前已经选取的元素个数为0（path.length===0）,那么 n-(k-path.length)+1 即 4-(3-0)+1=2，从2开始搜索是合理的，组合为【2,3,4】
-        for (let i = startIndex; i <= n - (k - path.length) + 1; i++) {
-            path.push(i);
+        for (let i = startIndex; i <= n; i++) {
+            path.push(i)
             backtracking(n, k, i + 1);
-            path.pop();
+            path.pop()
         }
 
     }
-
-
     backtracking(n, k, 1)
     return res;
-
-
-};
+}
 // @lc code=end
 
