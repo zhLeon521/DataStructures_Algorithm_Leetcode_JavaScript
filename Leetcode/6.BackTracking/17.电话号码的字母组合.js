62a51e28229d4fb7cdd004462bc556e95834fbba
zhleon <zholeon@qq.com>
2021-12-27 14:08:43 +0800

    电话号码的字母组合

--- Leetcode/6.BackTracking/17.电话号码的字母组合.js
+++ Leetcode/6.BackTracking/17.电话号码的字母组合.js
@@ -9,10 +9,7 @@
  * @param {string} digits
  * @return {string[]}
  */
-
-
-
-const letterCombinations = (digits) => {
+ const letterCombinations = (digits) => {
     if (digits.length === 0) return [];
     const map = {
         2: 'abc',
@@ -29,11 +26,11 @@ const letterCombinations = (digits) => {
     let path = [];
 
     // digits:传入的【数字】，如“23”，digits.length=2
-    // index:记录遍历第几个【数字】
+    // index:记录遍历第几个【数字】,即用来遍历题目中输入的字符串，同时index也表示树的深度
     const backtracking = (digits, index) => {
         if (digits.length === index) { //如果此时index等于【输入数字的个数】
             res.push(path.join("")) //将此条结果传入res中 ...看答案形式进行输出
-            return; //结束本层迭代
+            return; 
         }
         // 迭代循环：index
         for (const letter of map[digits[index]]) {
