5fbf3e7f0e8702bf76d431e2fcc6bd769d79563e
blueheart <2321837783@qq.com>
2022-02-01 21:02:16 +0800

    终于成了

---
+++ leetcode-3/3.Hash Table/18.四数之和.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=javascript
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    let res = [];
+    if (nums === null || nums.length < 4) return res;
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            let left = j + 1;
+            let right = nums.length - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum === target) {
+                    res.push([nums[i], nums[j], nums[left], nums[right]])
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                if (sum < target) {
+                    left++;
+                }
+                if (sum > target) {
+                    right--;
+                }
+            }
+        }
+    }
+    return res;
+
+}
+
+
+// @lc code=end
+
