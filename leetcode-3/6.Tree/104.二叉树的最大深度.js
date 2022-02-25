85543ef2d289f93fb58950135d090c14c2b0b55d
blueheart <2321837783@qq.com>
2022-02-25 11:10:03 +0800

    最大深度 递归

--- leetcode-3/6.Tree/104.二叉树的最大深度.js
+++ leetcode-3/6.Tree/104.二叉树的最大深度.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Autor: Blueheart
  * @Date: 2022-02-25 10:45:36
- * @LastEditTime: 2022-02-25 10:55:58
+ * @LastEditTime: 2022-02-25 10:58:02
  * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\leetcode-3\6.Tree\104.二叉树的最大深度.js
  */
 /*
@@ -25,23 +25,34 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    let queue = [];
-    queue.push(root);
-    if (root === null) return root;
+    // let queue = [];
+    // queue.push(root);
+    // if (root === null) return root;
 
-    let depth = 0;
+    // let depth = 0;
 
-    while (queue.length) {
-        let length = queue.length;
-        while (length--) {
-            let node = queue.shift();
-            if (node.left) queue.push(node.left)
-            if (node.right) queue.push(node.right)
-        }
-        depth++;
+    // while (queue.length) {
+    //     let length = queue.length;
+    //     while (length--) {
+    //         let node = queue.shift();
+    //         if (node.left) queue.push(node.left)
+    //         if (node.right) queue.push(node.right)
+    //     }
+    //     depth++;
+    // }
+
+    // return depth;
+
+
+
+    // *************方法二*********************
+    if (root === null) {
+        return null;
+    } else {
+        const left = maxDepth(root.left);
+        const right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
     }
-
-    return depth;
 
 };
 // @lc code=end
