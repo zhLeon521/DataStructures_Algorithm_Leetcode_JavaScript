6a481247a99b033df2f814ec5239efa9187ea22f
blueheart <2321837783@qq.com>
2022-02-25 10:56:48 +0800

    最大深度

---
+++ leetcode-3/6.Tree/104.二叉树的最大深度.js
@@ -0,0 +1,48 @@
+/*
+ * @Description: 
+ * @Autor: Blueheart
+ * @Date: 2022-02-25 10:45:36
+ * @LastEditTime: 2022-02-25 10:55:58
+ * @FilePath: \DataStructures_Algorithm_Leetcode_JavaScript\leetcode-3\6.Tree\104.二叉树的最大深度.js
+ */
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    let queue = [];
+    queue.push(root);
+    if (root === null) return root;
+
+    let depth = 0;
+
+    while (queue.length) {
+        let length = queue.length;
+        while (length--) {
+            let node = queue.shift();
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        depth++;
+    }
+
+    return depth;
+
+};
+// @lc code=end
+
