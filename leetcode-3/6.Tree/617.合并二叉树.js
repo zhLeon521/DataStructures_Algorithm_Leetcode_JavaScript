8999cc87b6fa2f1c549aeb77f62c5539c5abf79a
blueheart <2321837783@qq.com>
2022-02-27 01:26:05 +0800

    合并二叉树

---
+++ leetcode-3/6.Tree/617.合并二叉树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=javascript
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+    // 如果一棵树有，另一棵树没有，接上去
+    if (root1 === null) return root2;
+    if (root2 === null) return root1;
+    // 如果两个节点都存在，则对值求和
+    root1.val += root2.val;
+    // 递归合并左右子树
+    root1.left = mergeTrees(root1.left, root2.left);
+    root1.right = mergeTrees(root1.right, root2.right);
+    return root1;
+};
+// @lc code=end
+
